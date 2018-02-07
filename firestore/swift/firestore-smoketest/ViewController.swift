1092e4b0842cdd55b39bcd9b620b901faea53a37
Sam <samstern@google.com>
2018-02-07 11:17:00 -0800

    Document metadata options
    Change-Id: I53b944bacff04ea7e71d38f36de464d29ed7704b

--- firestore/swift/firestore-smoketest/ViewController.swift
+++ firestore/swift/firestore-smoketest/ViewController.swift
@@ -71,6 +71,7 @@ class ViewController: UIViewController {
         customClassGetDocument()
         listenDocument()
         listenDocumentLocal()
+        listenDocumentOptions()
         getMultiple()
         getMultipleAll()
         listenMultiple()
@@ -671,6 +672,18 @@ class ViewController: UIViewController {
                 print("\(source) data: \(document.data())")
             }
         // [END listen_document_local]
+    }
+
+    private func listenDocumentOptions() {
+      // [START listen_with_metadata]
+      // Listen to document metadata.
+      let options = DocumentListenOptions().includeMetadataChanges(true);
+
+      db.collection("cities").document("SF")
+        .addSnapshotListener(options:options, listener: { documentSnapshot, error in
+          // ...
+      })
+      // [END listen_with_metadata]
     }
 
     private func getMultiple() {
