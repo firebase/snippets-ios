1481aac34d172e3d30c5656178b9961fb95b16f9
Morgan Chen <morganchen12@gmail.com>
2025-11-19 15:32:14 -0800

    test deleting a snippet

--- appcheck/AppCheckSnippetsObjC/AppAttestProviderFactories.m
+++ appcheck/AppCheckSnippetsObjC/AppAttestProviderFactories.m
@@ -30,19 +30,4 @@
 @end
 // [END appcheck_simple_appattest_factory]
 
-// [START appcheck_appattest_factory]
-@interface YourAppCheckProviderFactory : NSObject <FIRAppCheckProviderFactory>
-@end
 
-@implementation YourAppCheckProviderFactory
-
-- (nullable id<FIRAppCheckProvider>)createProviderWithApp:(nonnull FIRApp *)app {
-  if (@available(iOS 14.0, *)) {
-    return [[FIRAppAttestProvider alloc] initWithApp:app];
-  } else {
-    return [[FIRDeviceCheckProvider alloc] initWithApp:app];
-  }
-}
-
-@end
-// [START appcheck_appattest_factory]
