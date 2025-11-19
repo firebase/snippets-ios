2e3aeafab791ee60561b9004ebdd7fc034e0c0bd
Morgan Chen <morganchen12@gmail.com>
2025-11-19 13:28:59 -0800

    move quickstart snippets to snippets repo (#417)

---
+++ qs-snippets/DatabaseExampleSwift/PostDataSource.swift
@@ -0,0 +1,50 @@
+//
+//  Copyright (c) 2015 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+import FirebaseDatabaseUI
+
+class PostDataSource: FUITableViewDataSource {
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+
+  override func tableView(_ tableView: UITableView,
+                          commit editingStyle: UITableViewCell.EditingStyle,
+                          forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      snapshot(at: indexPath.row).ref.removeValue()
+    }
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if count != 0 {
+      tableView.separatorStyle = .singleLine
+      tableView.backgroundView = nil
+    }
+    return Int(count)
+  }
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    let noDataLabel = UILabel(frame: CGRect(origin: .zero, size: tableView.bounds.size))
+    noDataLabel.text = "No posts yet - why not add one?"
+    noDataLabel.textColor = UIColor.black
+    noDataLabel.textAlignment = .center
+    tableView.backgroundView = noDataLabel
+    tableView.separatorStyle = .none
+    return 1
+  }
+}
---
+++ qs-snippets/DatabaseExampleSwift/SignInViewController.swift
@@ -0,0 +1,163 @@
+//
+//  Copyright (c) 2015 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
+@objc(SignInViewController)
+class SignInViewController: UIViewController, UITextFieldDelegate {
+  @IBOutlet var emailField: UITextField!
+  @IBOutlet var passwordField: UITextField!
+  var ref: DatabaseReference!
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    if Auth.auth().currentUser != nil {
+      performSegue(withIdentifier: "signIn", sender: nil)
+    }
+    ref = Database.database().reference()
+  }
+
+  // Saves user profile information to user database
+  func saveUserInfo(_ user: FirebaseAuth.User, withUsername username: String) {
+    // Create a change request
+    showSpinner {}
+    let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+    changeRequest?.displayName = username
+
+    // Commit profile changes to server
+    changeRequest?.commitChanges { error in
+
+      self.hideSpinner {}
+
+      if let error = error {
+        self.showMessagePrompt(error.localizedDescription)
+        return
+      }
+
+      // [START basic_write]
+      self.ref.child("users").child(user.uid).setValue(["username": username])
+      // [END basic_write]
+      self.performSegue(withIdentifier: "signIn", sender: nil)
+    }
+  }
+
+  @IBAction func didTapEmailLogin(_ sender: AnyObject) {
+    guard let email = emailField.text, let password = passwordField.text else {
+      showMessagePrompt("email/password can't be empty")
+      return
+    }
+
+    showSpinner {}
+
+    // Sign user in
+    Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+
+      self.hideSpinner {}
+
+      guard let user = authResult?.user, error == nil else {
+        self.showMessagePrompt(error!.localizedDescription)
+        return
+      }
+
+      self.ref.child("users").child(user.uid).observeSingleEvent(of: .value, with: { snapshot in
+
+        // Check if user already exists
+        guard !snapshot.exists() else {
+          self.performSegue(withIdentifier: "signIn", sender: nil)
+          return
+        }
+
+        // Otherwise, create the new user account
+        self.showTextInputPrompt(withMessage: "Username:") { userPressedOK, username in
+
+          guard let username = username else {
+            self.showMessagePrompt("Username can't be empty")
+            return
+          }
+
+          self.saveUserInfo(user, withUsername: username)
+        }
+      }) // End of observeSingleEvent
+    }) // End of signIn
+  }
+
+  @IBAction func didTapSignUp(_ sender: AnyObject) {
+    func getEmail(completion: @escaping (String) -> Void) {
+      showTextInputPrompt(withMessage: "Email:") { userPressedOK, email in
+        guard let email = email else {
+          self.showMessagePrompt("Email can't be empty.")
+          return
+        }
+        completion(email)
+      }
+    }
+
+    func getUsername(completion: @escaping (String) -> Void) {
+      showTextInputPrompt(withMessage: "Username:") { userPressedOK, username in
+        guard let username = username else {
+          self.showMessagePrompt("Username can't be empty.")
+          return
+        }
+        completion(username)
+      }
+    }
+
+    func getPassword(completion: @escaping (String) -> Void) {
+      showTextInputPrompt(withMessage: "Password:") { userPressedOK, password in
+        guard let password = password else {
+          self.showMessagePrompt("Password can't be empty.")
+          return
+        }
+        completion(password)
+      }
+    }
+
+    // Get the credentials of the user
+    getEmail { email in
+      getUsername { username in
+        getPassword { password in
+
+          // Create the user with the provided credentials
+          Auth.auth()
+            .createUser(withEmail: email, password: password, completion: { authResult, error in
+
+              guard let user = authResult?.user, error == nil else {
+                self.showMessagePrompt(error!.localizedDescription)
+                return
+              }
+
+              // Finally, save their profile
+              self.saveUserInfo(user, withUsername: username)
+
+            })
+        }
+      }
+    }
+  }
+
+  // MARK: - UITextFieldDelegate protocol methods
+
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    didTapEmailLogin(textField)
+    return true
+  }
+}
